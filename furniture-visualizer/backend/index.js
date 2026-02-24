8e7991c2a6641a0de55d7c09c575b9a5120366dc
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2026-02-24 15:14:03 +0530

    feat: implement JWT authentication middleware for user authorization

--- furniture-visualizer/backend/index.js
+++ furniture-visualizer/backend/index.js
@@ -2,10 +2,29 @@ import 'dotenv/config';
 import bodyParser from 'body-parser';
 import express from 'express';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 import userRouter from './routes/userrouter.js';
 
 const app = express();
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+    const value = req.headers['authorization'];
+    if(value !== null){
+       const token = value.replace('Bearer ', '');
+       jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if(decoded == null){
+            return res.status(401).json({ message: "Unauthorized" , error: err.message });
+    }else{
+        req.user = decoded;
+        next();
+    }
+         })
+    }else{
+        next();
+    }
+
+});
+
+
 app.use('/api', userRouter);
 
 const connectionString =  process.env.MONGODB_URI;
