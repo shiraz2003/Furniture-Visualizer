45ec9931861f53c368e85e812e4c9976a4a80599
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2026-02-24 15:06:20 +0530

    feat: add user authentication endpoints and update dependencies
    - Added bcrypt and jsonwebtoken for password hashing and token generation.
    - Updated package.json to include new dependencies.
    - Implemented loginUser function in user router for user authentication.

--- furniture-visualizer/backend/controller/usercontroller.js
+++ furniture-visualizer/backend/controller/usercontroller.js
@@ -1,7 +1,19 @@
+import express from "express";
 import User from "../model/usermodel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 export function createUser(req, res) {
-  const user = new User(req.body);
+    const passwordHash = bcrypt.hashSync(req.body.password, 10);
+    const userData = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: passwordHash,
+        role: req.body.role || "user",
+    };
+  const user = new User(userData);
   user
     .save()
     .then(() => res.status(201).json({ message: "User created successfully" }))
@@ -11,4 +23,39 @@ export function createUser(req, res) {
         error: error.message,
       }),
     );
+}
+
+export function loginUser(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email: email })
+        .then((user) => {
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }else{
+            const isPasswordValid = bcrypt.compareSync(password, user.password);
+            if(isPasswordValid){
+                const token = jwt.sign({
+                    email: user.email,
+                    firstname: user.firstname,
+                    lastname: user.lastname,
+                    role: user.role
+                 }, process.env.JWT_SECRET, { expiresIn: '1h'
+                })
+                res.status(200).json({ message: "Login successful", token: token, user: user });
+            }else{
+                res.status(401).json({ message: "Invalid password" });
+            }
+        }})
+        .catch((error) =>
+            res.status(500).json({
+                message: "Error logging in",
+                error: error.message,
+            }),
+        );
+
+           
+    
+        
 }
--- furniture-visualizer/backend/routes/userrouter.js
+++ furniture-visualizer/backend/routes/userrouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createUser } from '../controller/usercontroller.js';
+import { createUser, loginUser } from '../controller/usercontroller.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/users', createUser);
+userRouter.post('/login', loginUser);
 
 export default userRouter;
