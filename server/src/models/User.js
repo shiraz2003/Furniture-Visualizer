48918d1cba388e429129da6943e1d95b5bd30ef6
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2026-02-26 21:56:21 +0530

    Update user role schema to include 'designer' and change default role to 'customer'

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -20,8 +20,8 @@ const userSchema = new mongoose.Schema({
     },
     role : {
         type : String,
-        enum : ['user', 'admin'],
-        default : 'user'
+        enum : ['customer', 'admin', 'designer'],
+        default : 'customer'
     }
 }, {timestamps : true}
 );
